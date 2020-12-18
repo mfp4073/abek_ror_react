5381284861d94f41aac7db680d450569fbaf6c0a
Abe Kinney <abe@twtmob.com>
2020-12-17 16:11:28 -0800

    routing working

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Airline from './Airline/Airline'
+import Airlines from './Airlines/Airlines'
 
 const App = () => {
   return(
     <div className="App">
-      <header>
-      </header>
-      <Switch>
+      < Switch >
         <Route exact path="/" component={Airlines} />
-        <Route exact path="/airlines/:slug" component={Airlines} />
-      </Switch>
+        <Route exact path="/airline/:slug" component={Airline} />
+      </Switch >
     </div>
   )
 }
